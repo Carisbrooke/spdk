13223e5184a7caa5c26f21d2852af2b88c89ef76
repu1sion <bearrailgun@gmail.com>
2019-01-24 22:17:42 +0000

    building spdk as .so

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-./configure --with-dpdk=/root/dpdk/dpdk/x86_64-native-linuxapp-gcc --with-rbd
-make
+./configure --with-dpdk=/root/dpdk/dpdk/x86_64-native-linuxapp-gcc --with-rbd --with-shared
+make -j7
 if [ $? -eq 0 ]; then
+	make install
         ldconfig
         echo "build ok"
 else
